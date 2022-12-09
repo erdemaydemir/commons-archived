d40334df60d14937d8a9e10188a66468dfe13218
Pova <povatr@gmail.com>
2022-12-09 14:41:59 +0300

    updated.

--- src/main/java/com/forguta/commons/util/DozerMapper.java
+++ src/main/java/com/forguta/commons/util/DozerMapper.java
@@ -1,10 +1,8 @@
 package com.forguta.commons.util;
 
-import lombok.experimental.UtilityClass;
 import org.dozer.DozerBeanMapper;
 import org.springframework.stereotype.Component;
 
 @Component
-@UtilityClass
 public class DozerMapper extends DozerBeanMapper {
 }
