d3669d4c85187bdcad7a60c2aef6eb02c5d19db0
Pova <povatr@gmail.com>
2022-12-10 23:10:22 +0300

    adding ObjectMapper.

---
+++ src/main/java/com/forguta/commons/util/MyObjectMapper.java
@@ -0,0 +1,8 @@
+package com.forguta.commons.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyObjectMapper extends ObjectMapper {
+}
